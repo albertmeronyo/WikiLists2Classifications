6c7def05305eb1ed6085103caab37127f22fcc39
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2014-06-23 15:41:43 +0200

    Fix #4

--- parseWiki.py
+++ parseWiki.py
@@ -3,25 +3,46 @@
 import urllib
 import argparse
 import json
+import logging
+from rdflib import Graph
 from HTMLParser import HTMLParser
 
-# The extracted taxonomy goes into a dict
-class Taxonomy:
+# The extracted classification goes into a dict
+class Classification:
     def __init__(self):
-        self.taxonomy = {}
+        self.idsToLabels = {}
+        self.classification = {}
+        self.depth = {}
 
-    def addEntry(self, parent, child):
-        if not parent in self.taxonomy:
-            self.taxonomy[parent] = []
-        self.taxonomy[parent].append(child)
+    def addEntry(self, parent, child, depth):
+        if not parent in self.classification:
+            self.classification[parent] = []
+        self.classification[parent].append(child)
+        self.depth[parent] = depth
+        self.depth[child] = depth + 1
 
-    def layout(self):
-        print(json.dumps(self.taxonomy, indent=4))        
+    def getRoot(self):
+        return self.classification[None][0]
 
+    def layoutJSON(self):        
+        print(json.dumps(self.classification, indent=4))
+
+class ClassificationSerializer:
+    def __init__(self, __classification):
+        self.classification = __classification
+        self.graph = Graph()
+
+    def toSKOS(self):
+        print 'toSKOS'
+
+    def toQB(self):
+        print 'toQB'
+    
 # create a subclass and override the handler methods
-class TaxonomyHTMLParser(HTMLParser):
-    def __init__(self, __taxonomy):
+class ClassificationHTMLParser(HTMLParser):
+    def __init__(self, __classification):
         HTMLParser.__init__(self)
+        self.endParsing = False
         self.titleFound = False
         self.entryFound = False
         self.currentList = 0
@@ -29,7 +50,7 @@ class TaxonomyHTMLParser(HTMLParser):
         self.currentLevel = 0
         self.currentData = None
         self.parentStack = []
-        self.taxonomy = __taxonomy
+        self.classification = __classification
 
     def handle_starttag(self, tag, attrs):
         if tag == 'li':
@@ -39,8 +60,11 @@ class TaxonomyHTMLParser(HTMLParser):
             self.titleFound = True
             self.previousLevel = self.currentLevel
             self.currentLevel = int(tag[-1])
-            if self.currentLevel > self.previousLevel:
+            levelDiff = self.currentLevel - self.previousLevel
+            if levelDiff > 0:
                 self.parentStack.append(self.currentData)
+            elif levelDiff < 0:
+                self.parentStack.pop()
         if tag == 'ul':
             self.currentList += 1
             self.currentLevel += 1
@@ -55,41 +79,75 @@ class TaxonomyHTMLParser(HTMLParser):
             self.titleFound = False
 
     def handle_data(self, data):
-        if self.titleFound:
+        if self.titleFound and not self.endParsing:
             self.currentData = data
-            if not self.isUninteresting(self.parentStack[-1]):
-                self.taxonomy.addEntry(self.parentStack[-1], self.currentData)
+            logging.debug(self.currentData)
+            logging.debug(self.parentStack[-1])
+            logging.debug(self.currentLevel)
+            if not self.isUninteresting(self.parentStack[-1]) and not self.isUninteresting(self.currentData):
+                self.classification.addEntry(self.parentStack[-1], self.currentData, self.currentLevel)
             self.titleFound = False
-        if self.currentList and self.entryFound:
+        if self.currentList and self.entryFound and not self.endParsing:
             self.currentData = data
-            if not self.isUninteresting(self.parentStack[-1]):
-                self.taxonomy.addEntry(self.parentStack[-1], self.currentData)
+            logging.debug(self.currentData)
+            logging.debug(self.parentStack[-1])
+            logging.debug(self.currentLevel)
+            if not self.isUninteresting(self.parentStack[-1]) and not self.isUninteresting(self.currentData):
+                self.classification.addEntry(self.parentStack[-1], self.currentData, self.currentLevel)
             self.entryFound = False
+
+    def handle_comment(self, data):
+        if data.find('parser cache') >= 0:
+            self.endParsing = True
 
     def isHeader(self, tag):
         return tag in ('h1', 'h2', 'h3', 'h4', 'h5', 'h6')
 
     def isUninteresting(self, data):
-        return data in ('Content', 'Navigation menu', 'See also')
-
-
+        return data in ('Contents', 'Navigation menu', 'See also', 'External links', 'References')
+    
 if __name__ == "__main__":
+    # Parse commandline arguments
     parser = argparse.ArgumentParser(description="Extract SKOS taxonomies from Wikipedia pages")
     parser.add_argument('--input', '-i',
-                    help = "URL of the source Wikipedia page", 
-                    required = True)
-    
+                        help = "URL of the source Wikipedia page", 
+                        required = True)
+    parser.add_argument('--verbose', '-v',
+                        help = "Be verbose -- debug logging level",
+                        required = False, 
+                        action = 'store_true')
+    parser.add_argument('--format', '-f',
+                        help = "Output format: SKOS, QB",
+                        required = True,
+                        choices = ['SKOS', 'QB'])
+    parser.add_argument('--output', '-o',
+                        help = "Output filename",
+                        required = True)
     args = parser.parse_args()
 
+    # Logging
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    logging.info('Reading remote URL %s...' % args.input)
     response = urllib.urlopen(args.input)
     html = response.read()
 
-    taxonomy = Taxonomy()
+    logging.info('Initializing local data structure...')
+    classification = Classification()
 
-    # instantiate the parser and fed it some HTML
-    HTMLparser = TaxonomyHTMLParser(taxonomy)
+    logging.info('Parsing HTML...')
+    # instantiate the HTML parser
+    HTMLparser = ClassificationHTMLParser(classification)
     HTMLparser.feed(html)
 
-    # print html
-    taxonomy.layout()
+    logging.info('Converting to %s and serializing to %s...' % (args.format, args.output))
+    converter = ClassificationSerializer(classification)
+    if args.format == 'SKOS':
+        converter.toSKOS()
+    elif args.format == 'QB':
+        converter.toQB()
 
+    logging.info('Done.')
